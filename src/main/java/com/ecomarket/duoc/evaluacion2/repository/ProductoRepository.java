d49410a4eee52d4641a785d3e70e5dec4d97eeb0
Jose-Duoc24 <jcarrascov96@gmail.com>
2025-05-21 19:11:39 -0400

    Commit Jose ¯Repository

--- src/main/java/com/ecomarket/duoc/evaluacion2/repository/ProductoRepository.java
+++ src/main/java/com/ecomarket/duoc/evaluacion2/repository/ProductoRepository.java
@@ -4,6 +4,13 @@ import com.ecomarket.duoc.evaluacion2.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+    List<Producto>findByNombre(String nombre);
+
+    Producto findByMarca(String marca);
+
+    List<Producto>findByNombreyMarca(String nombre,String marca);
+
 }
