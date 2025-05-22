99ed7e32566054c8e81b684315521773474e47b2
Jose-Duoc24 <jcarrascov96@gmail.com>
2025-05-21 20:13:33 -0400

    Commit Jose ¯Repository + Controller

---
+++ src/main/java/com/ecomarket/duoc/evaluacion2/Controller/PacienteController.java
@@ -0,0 +1,17 @@
+package com.ecomarket.duoc.evaluacion2.Controller;
+
+import com.ecomarket.duoc.evaluacion2.model.Producto;
+import com.ecomarket.duoc.evaluacion2.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class PacienteController {
+}
--- src/main/java/com/ecomarket/duoc/evaluacion2/repository/ProductoRepository.java
+++ src/main/java/com/ecomarket/duoc/evaluacion2/repository/ProductoRepository.java
@@ -2,15 +2,22 @@ package com.ecomarket.duoc.evaluacion2.repository;
 
 import com.ecomarket.duoc.evaluacion2.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    List<Producto>findByNombre(String nombre);
+    //Usando JPQL
+    @Query("SELECT p FROM Producto p Where p.nombre=nombre")
+    List<Producto>buscarPorNombre(@Param("Nombre")String nombre);
 
-    Producto findByMarca(String marca);
+    //Usando SQL nativo
+    @Query(value = "SELECT * FROM producto Where marca= :marca",nativeQuery = true)
+    Producto buscarPorMarca(@Param("marca")String marca);
 
-    List<Producto>findByNombreyMarca(String nombre,String marca);
+    List<Producto>buscarPorNombreyMarca(String nombre,String marca);
 
 }
