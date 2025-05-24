fe6b2c95e4ca8de3fe00351bbabed0ba3138e594
04kryzz <cr.cruzs@duocuc.cl>
2025-05-24 19:50:51 -0400

    Commit Cristobal

--- src/main/java/com/ecomarket/duoc/evaluacion2/Controller/ProductoController.java
+++ src/main/java/com/ecomarket/duoc/evaluacion2/Controller/ProductoController.java
@@ -13,4 +13,57 @@ import java.util.List;
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
 
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> listar() {
+        List<Producto> productos = productoService.findAll();
+        if (productos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(productos);
+    }
+
+    @PostMapping
+    public ResponseEntity<Producto> guardar(@RequestBody Producto producto) {
+        Producto productoNuevo = productoService.save(producto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productoNuevo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Producto> buscar(@PathVariable Integer id) {
+        try {
+            Producto producto = productoService.findById(id);
+            return ResponseEntity.ok(producto);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Producto> actualizar(@PathVariable Integer id, @RequestBody Producto producto) {
+        try {
+            Producto prodExistente = productoService.findById(id);
+            prodExistente.setNombre(producto.getNombre());
+            prodExistente.setMarca(producto.getMarca());
+            prodExistente.setValor(producto.getValor());
+            prodExistente.setCantidad(producto.getCantidad());
+
+            productoService.save(prodExistente);
+            return ResponseEntity.ok(prodExistente);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        try {
+            productoService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
